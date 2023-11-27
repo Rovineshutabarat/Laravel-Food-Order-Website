6ca0024bf80c55c7514be616639403c28c9f4c49
Rovines <rovineshutabarat23@gmail.com>
2023-11-27 17:03:17 +0700

    First Commit - Fresh Project Setup

---
+++ resources/views/livewire/auth/login.blade.php
@@ -0,0 +1,11 @@
+@push('style')
+    @livewireStyles
+@endpush
+
+@push('script')
+    @livewireScripts
+@endpush
+
+<div>
+    Login Page
+</div>
---
+++ resources/views/static/index.blade.php
@@ -0,0 +1,5 @@
+@extends('layouts.static')
+
+@section('pages')
+    <h1>Homepage</h1>
+@endsection
